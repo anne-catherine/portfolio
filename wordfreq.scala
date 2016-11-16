ecdf1edd80a0a57039e90a0665a3f69ce581f208
Anne Thompson <annethompson@Annes-MacBook-Pro-2.local>
2016-11-15 20:48:11 -0500

    Refined wordfreq.scala

--- wordfreq.scala
+++ wordfreq.scala
@@ -10,19 +10,11 @@ object WordFreq {
 
     val lines = Source.fromFile(args(0)).getLines.toVector
 
-    println("Read "  + lines.size + " sections")
-
     val txt = lines.mkString
-
-    println ("Read " + txt.size + " characters.")
 
     val words = txt.split("\\W+").filterNot(_.isEmpty)
 
-    println("Read " + words.size + " words.")
-
     val grouped = words.groupBy(w => w)
-
-    println ("Read " + grouped.size + " unique words.")
 
     val freqs = grouped.map { case (k,v) => (k,v.size) }
 
@@ -31,6 +23,11 @@ object WordFreq {
     for (wd <- sorted ) {
      println(wd)
      }
+
+println("Read " + lines.size + " sections.")
+println ("Read " + txt.size + " characters.")
+println("Read " + words.size + " words.")
+println ("Read " + grouped.size + " unique words.")
 
   }
 
