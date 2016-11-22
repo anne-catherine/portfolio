7afcb52be1074c0cd5bbf8b36b0883aa2bea0bfb
Anne Thompson <annethompson@Annes-MacBook-Pro-2.local>
2016-11-22 14:14:08 -0500

    Added first version of align.scala

---
+++ align.scala
@@ -0,0 +1,29 @@
+import scala.io.Source
+
+
+def aligned(s: String, parsedList: Vector[Array[String]] ) = {
+    val parsedWord = parsedList.filter(_(0) == s)
+    if (parsedWord.size == 0) {
+      s
+    } else {
+      parsedWord.map(_(1)).mkString(",")
+    }
+  }
+
+
+  val text = Source.fromFile("dbc.txt").getLines.toVector
+  val textSplit = text.map(_.split("\t"))git 
+  val sections = textSplit.map(_(1))
+  val reff = textSplit.map(_(0))
+
+
+  val parsedList = Source.fromFile("ParsedWords.txt").getLines.toVector
+  val wordList = parsedList.map(_.split("\t")).filter(_.size == 2 )
+
+
+  val alignedText = sections.map { s =>
+    val words = s.split("\\W").filterNot(_.isEmpty)
+    words.map ( w => aligned(w,wordList) ).mkString(" ")
+  }
+
+ println(alignedText)
